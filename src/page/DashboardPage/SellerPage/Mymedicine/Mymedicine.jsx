cb2170c34dc4dfc156f12ed03f1819033132e84a
mdsamiulislam54 <samiulm5332@gmail.com>
2025-07-12 00:09:38 +0600

    feat: add pagination functionality for medicine list with dynamic page controls

--- src/page/DashboardPage/SellerPage/Mymedicine/Mymedicine.jsx
+++ src/page/DashboardPage/SellerPage/Mymedicine/Mymedicine.jsx
@@ -1,63 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import axiosinstance from "../../../../hooks/axiosInstance/axiosinstance";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../../../../components/Loader/Loader";
 
 const Mymedicine = () => {
   const axiosInstanceCall = axiosinstance();
+  const [count, setCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+  const totalPage = Math.ceil(count / perPage) || 0;
+  const pageArray = [...Array(totalPage).keys()];
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ["medicine"],
+    queryKey: ["medicine", currentPage, perPage],
     queryFn: async () => {
-      const res = await axiosInstanceCall.get("/all-medicine");
+      const res = await axiosInstanceCall.get(
+        `all-medicine?limit=${perPage}&page=${currentPage}`
+      );
       console.log(res);
+      setCount(res.data.count);
       return res.data.result;
     },
   });
 
-  if (isLoading) {
-    return <Loader />;
-  }
+
   if (error) {
     return <p>{error.message}</p>;
   }
-
+  console.log(count);
+  console.log(totalPage);
   return (
     <div>
       <div className="px-4">
-        <h2>Total Medicines: {data.length}</h2>
+        <h2>Total Medicines: {count}</h2>
 
         <div>
-          <div className="overflow-x-auto">
-            <table className="table table-zebra">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>GenericName</th>
-                  <th>Category</th>
-                  <th>Price</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((medicine) => {
-                  return (
+          {data?.length === 0 ? (
+            <p>No medicines found.</p>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="table table-zebra">
+                <thead>
+                  <tr className="bg-secondary">
+                    <th></th>
+                    <th>Name</th>
+                    <th>GenericName</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {isLoading ? (
                     <tr>
-                      <th>
-                        <img src={medicine.imageURL} alt="" className="w-8 h-8 object-contain" />
-                      </th>
-                      <td>{medicine.name}</td>
-                      <td>{medicine.genericName}</td>
-                      <td>{medicine.category}</td>
-                      <td>{medicine.price}</td>
-                      <td>x</td>
+                      <td colSpan="6">
+                        <div className="flex justify-center items-center h-20 min-h-screen">
+                          <Loader />
+                        </div>
+                      </td>
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                  ) : (
+                    data.map((medicine) => (
+                      <tr key={medicine._id} className="hover:bg-secondary">
+                        <th>
+                          <img
+                            src={medicine.imageURL}
+                            alt=""
+                            className="max-w-[40px] h-auto object-contain"
+                          />
+                        </th>
+                        <td>{medicine.name}</td>
+                        <td>{medicine.genericName}</td>
+                        <td>{medicine.category}</td>
+                        <td>{medicine.price}</td>
+                        <td>x</td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+            </div>
+          )}
+
+          <div className="flex justify-center items-center my-10">
+            <button
+              className="btn mx-4"
+              disabled={currentPage === 0}
+              onClick={() => setCurrentPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+            <ul className="flex gap-4">
+              {pageArray?.map((page) => {
+                return (
+                  <li
+                    key={page}
+                    className={`btn bg-gray-200 ${
+                      currentPage === page ? "bg-primary text-white" : ""
+                    }`}
+                    onClick={() => setCurrentPage(page)}
+                  >
+                    {page + 1}
+                  </li>
+                );
+              })}
+              <button
+                className="btn mx-4"
+                disabled={pageArray?.length - 1 === currentPage ? true : false}
+                onClick={() => setCurrentPage((prev) => prev + 1)}
+              >
+                Next
+              </button>
+            </ul>
           </div>
         </div>
       </div>
